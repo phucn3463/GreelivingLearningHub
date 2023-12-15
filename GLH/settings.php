6ccbe05a7e545fb2d4e2030054ef4ef006819977
phucn3463 <114555549+phucn3463@users.noreply.github.com>
2023-12-15 13:08:26 +0700

    Delete GLH directory

--- GLH/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$host = "localhost";
-	$user = "root"; // your user name
-	$pwd = ""; // your password (date of birth ddmmyy unless changed)
-	$sql_db = "cos20031_final"; // your database
-?>
